c1572992d805ea21d27119f80d4cf52574d08450
baecheolhyeon <baecheolhyeon7@gmail.com>
2024-10-10 07:58:49 +0900

    [start]환경세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,39 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { LoggingInterceptor } from './common/interceptors/logging-interceptor';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CustomExceptionFilter } from './common/filters/exception-filter';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const env = configService.get<string>('NODE_ENV');
+        const synchronize = configService.get<boolean>('DB_SYNC_OPTION');
+
+        return {
+          type: 'postgres',
+          database: configService.get<string>('DB_NAME'),
+          host: configService.get<string>('DB_HOST'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASSWORD'),
+          port: configService.get<number>('DB_PORT'),
+          entities: [],
+          timezone: 'z',
+          synchronize: synchronize,
+          logging: env === 'local' ? true : false,
+        };
+      },
+
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature(),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
@@ -18,6 +44,15 @@ import { CustomExceptionFilter } from './common/filters/exception-filter';
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_PIPE,
+      useFactory: () => {
+        return new ValidationPipe({
+          whitelist: true,
+          transform: true,
+        });
+      },
     },
   ],
 })
