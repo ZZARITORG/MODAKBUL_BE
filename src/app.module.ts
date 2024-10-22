e72a5e38d288b34634448eba4764dcac44fb35eb
baecheolhyeon <baecheolhyeon7@gmail.com>
2024-10-22 21:31:10 +0900

    [start] 모듈구조 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,33 +6,45 @@ import { LoggingInterceptor } from './common/interceptors/logging-interceptor';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CustomExceptionFilter } from './common/filters/exception-filter';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './modules/auth/auth.module';
+import { MeetingModule } from './modules/meeting/meeting.module';
+import { NotificationModule } from './modules/notification/notification.module';
+import { FriendModule } from './modules/friend/friend.module';
+import { GroupModule } from './modules/group/group.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        const env = configService.get<string>('NODE_ENV');
-        const synchronize = configService.get<boolean>('DB_SYNC_OPTION');
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: (configService: ConfigService) => {
+    //     const env = configService.get<string>('NODE_ENV');
+    //     const synchronize = configService.get<boolean>('DB_SYNC_OPTION');
 
-        return {
-          type: 'postgres',
-          database: configService.get<string>('DB_NAME'),
-          host: configService.get<string>('DB_HOST'),
-          username: configService.get<string>('DB_USER'),
-          password: configService.get<string>('DB_PASSWORD'),
-          port: configService.get<number>('DB_PORT'),
-          entities: [],
-          timezone: 'z',
-          synchronize: synchronize,
-          logging: env === 'local' ? true : false,
-        };
-      },
+    //     return {
+    //       type: 'postgres',
+    //       database: configService.get<string>('DB_NAME'),
+    //       host: configService.get<string>('DB_HOST'),
+    //       username: configService.get<string>('DB_USER'),
+    //       password: configService.get<string>('DB_PASSWORD'),
+    //       port: configService.get<number>('DB_PORT'),
+    //       entities: [],
+    //       timezone: 'z',
+    //       synchronize: synchronize,
+    //       logging: env === 'local' ? true : false,
+    //     };
+    //   },
 
-      inject: [ConfigService],
-    }),
+    //   inject: [ConfigService],
+    // }),
     TypeOrmModule.forFeature(),
+    AuthModule,
+    MeetingModule,
+    NotificationModule,
+    FriendModule,
+    GroupModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api/v0');
   const config = new DocumentBuilder().setTitle('MODAKBUL SERVICE DOCS').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/docs', app, document, {
